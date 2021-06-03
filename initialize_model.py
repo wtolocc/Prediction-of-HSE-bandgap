76233bd70b8535c99fab3cfc4082ff15152a2264
wtolocc <82740579+wtolocc@users.noreply.github.com>
2021-06-03 21:41:52 +0800

    Add files via upload

--- initialize_model.py
+++ initialize_model.py
@@ -1,11 +1,28 @@
-from sklearn.ensemble import AdaBoostRegressor,GradientBoostingRegressor,RandomForestRegressor
+#2021.06.02
+from sklearn.ensemble import GradientBoostingRegressor,RandomForestRegressor
 from sklearn.linear_model import LinearRegression
 import pickle
 from sklearn.model_selection import train_test_split
 from load_store_dataframe import load_dataframe_from_json
 import joblib
+from sklearn.ensemble import GradientBoostingClassifier, RandomForestClassifier
 
 
+def model1_traning_classifier_3d():
+    model = RandomForestClassifier()
+
+    data = load_dataframe_from_json('model/data_feature_for_classify.json')
+    data = data.dropna()
+
+    features_rank = pickle.load(open('model/features_rank_class_metalORnometal_e.txt', 'rb'))
+    features = features_rank[:21]
+
+    X = data[features]
+    y = data['is semi'].values
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
+
+    model.fit(X_train, y_train)
+    joblib.dump(model, 'model/model_classify_metalORnometal.pkl')
 
 def model1_traning_3d():
 
@@ -62,6 +79,24 @@ def model3_traning_3d():
     model.fit(X_train, y_train)
     joblib.dump(model,'model/model_eANDgap.pkl')
 
+def model1_traning_classifier_2d():
+
+    model = GradientBoostingClassifier(learning_rate=0.2,max_depth=4,min_samples_leaf=2,min_samples_split=2,
+                                       n_estimators=70)
+
+    data = load_dataframe_from_json('model/data_feature_c2db.json')
+    data = data.dropna()
+
+    features_rank = pickle.load(open('model/features_rank_class_metalORnometal_c2db_e.txt', 'rb'))
+    features = features_rank[:14]
+
+    X = data[features]
+    y = data['is semi'].values
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
+
+    model.fit(X_train, y_train)
+    joblib.dump(model, 'model/model_classify_metalORnometal_c2db.pkl')
+
 def model1_traning_2d():
 
     model = RandomForestRegressor(
@@ -77,7 +112,7 @@ def model1_traning_2d():
     features = features_rank[:16]
 
     X = data[features]
-    y = data['gap_hse'].values
+    y = data['Band_gap_HSE'].values
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
 
     model.fit(X_train, y_train)
@@ -86,10 +121,11 @@ def model1_traning_2d():
 def model2_traning_2d():
 
     data=load_dataframe_from_json('model/data_feature_c2db.json')
+    data=data[data['Band_gap_HSE']>0]
 
 
     X = data['Band_gap_GGA'].values.reshape(-1,1)
-    y = data['gap_hse'].values
+    y = data['Band_gap_HSE'].values
 
     model = LinearRegression()
     model.fit(X,y)
@@ -111,16 +147,18 @@ def model3_traning_2d():
     features = features_rank[:21]
 
     X = data[features]
-    y = data['gap_hse'].values
+    y = data['Band_gap_HSE'].values
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
 
     model.fit(X_train, y_train)
     joblib.dump(model,'model/model_c2db_eANDgap.pkl')
 
 if __name__ == '__main__':
+    model1_traning_classifier_3d()
     model1_traning_3d()
     model2_traning_3d()
     model3_traning_3d()
+    model1_traning_classifier_2d()
     model1_traning_2d()
     model2_traning_2d()
     model3_traning_2d()
--- main.py
+++ main.py
@@ -6,6 +6,23 @@ from matminer.featurizers.conversions import StrToComposition
 import pandas as pd
 from load_store_dataframe import store_dataframe_as_json, load_dataframe_from_json
 
+def model_classifier_3d(formula):
+    data = pd.DataFrame({'formula': [formula]})
+
+    data = StrToComposition(target_col_id='composition_obj').featurize_dataframe(data, 'formula')
+    feature_calculators = MultipleFeaturizer([cf.Stoichiometry(), cf.ElementProperty.from_preset("magpie"),
+                                              cf.ValenceOrbital(props=['avg']), cf.IonProperty(fast=True)])
+    data = feature_calculators.featurize_dataframe(data, col_id='composition_obj', ignore_errors=True)
+
+    ##
+    features_rank = pickle.load(open('model/features_rank_class_metalORnometal_e.txt', 'rb'))
+    features = features_rank[:21]
+    # 读取模型
+    model = joblib.load('model/model_classify_metalORnometal.pkl')
+    X = data[features]
+    data['is semi'] = model.predict(X)
+    return data['is semi'].values[0]
+
 def model_1_3d(formula):
     data = pd.DataFrame({'formula': [formula]})
 
@@ -14,10 +31,10 @@ def model_1_3d(formula):
                                               cf.ValenceOrbital(props=['avg']), cf.IonProperty(fast=True)])
     data = feature_calculators.featurize_dataframe(data, col_id='composition_obj', ignore_errors=True)
 
-    
+    ##
     features_rank = pickle.load(open('model/features_rank_e.txt', 'rb'))
     features = features_rank[:30]
-    
+    # 读取模型
     model = joblib.load('model/model_e.pkl')
     X = data[features]
     data['predict_Band_gap_HSE'] = model.predict(X)
@@ -38,10 +55,10 @@ def model_3_3d(formula,bandgap_pbe):
                                               cf.ValenceOrbital(props=['avg']), cf.IonProperty(fast=True)])
     data = feature_calculators.featurize_dataframe(data, col_id='composition_obj', ignore_errors=True)
 
-    
+    ##
     features_rank = pickle.load(open('model/features_rank_eANDgap.txt', 'rb'))
     features = features_rank[:22]
-    
+    # 读取模型
     model = joblib.load('model/model_eANDgap.pkl')
     X = data[features]
     data['predict_Band_gap_HSE'] = model.predict(X)
@@ -52,17 +69,35 @@ def model_3_3d(formula,bandgap_pbe):
 def main_ML_3d(formula,bandgap_pbe):
     if  formula:
         if bandgap_pbe:
-
             model_3_3d(formula,bandgap_pbe)
             print('The model has score=0.96,RMSE=0.28')
         else:
-
-            model_1_3d(formula)
-            print('The model has score=0.76,RMSE=0.75')
+            res=model_classifier_3d(formula)
+            if res>0:
+                model_1_3d(formula)
+                print('The model has score=0.76,RMSE=0.75')
+            else:
+                print('formula : {}\n Bandgap: 0'.format(formula))
     else:
-
         model_2_3d(bandgap_pbe)
         print('The model has score=0.92,RMSE=0.43')
+
+def model_classifier_2d(formula):
+    data = pd.DataFrame({'formula': [formula]})
+
+    data = StrToComposition(target_col_id='composition_obj').featurize_dataframe(data, 'formula')
+    feature_calculators = MultipleFeaturizer([cf.Stoichiometry(), cf.ElementProperty.from_preset("magpie"),
+                                              cf.ValenceOrbital(props=['avg']), cf.IonProperty(fast=True)])
+    data = feature_calculators.featurize_dataframe(data, col_id='composition_obj', ignore_errors=True)
+
+    ##
+    features_rank = pickle.load(open('model/features_rank_class_metalORnometal_c2db_e.txt', 'rb'))
+    features = features_rank[:14]
+    # 读取模型
+    model = joblib.load('model/model_classify_metalORnometal_c2db.pkl')
+    X = data[features]
+    data['is semi'] = model.predict(X)
+    return data['is semi'].values[0]
 
 def model_1_2d(formula):
     data = pd.DataFrame({'formula': [formula]})
@@ -72,10 +107,10 @@ def model_1_2d(formula):
                                               cf.ValenceOrbital(props=['avg']), cf.IonProperty(fast=True)])
     data = feature_calculators.featurize_dataframe(data, col_id='composition_obj', ignore_errors=True)
 
-    
+    ##
     features_rank = pickle.load(open('model/features_rank_c2db_e.txt', 'rb'))
     features = features_rank[:16]
-  
+    # 读取模型
     model = joblib.load('model/model_c2db_e.pkl')
     X = data[features]
     data['predict_Band_gap_HSE'] = model.predict(X)
@@ -96,10 +131,10 @@ def model_3_2d(formula,bandgap_pbe):
                                               cf.ValenceOrbital(props=['avg']), cf.IonProperty(fast=True)])
     data = feature_calculators.featurize_dataframe(data, col_id='composition_obj', ignore_errors=True)
 
-    
+    ##
     features_rank = pickle.load(open('model/features_rank_c2db_eANDgap.txt', 'rb'))
     features = features_rank[:21]
-    
+    # 读取模型
     model = joblib.load('model/model_c2db_eANDgap.pkl')
     X = data[features]
     data['predict_Band_gap_HSE'] = model.predict(X)
@@ -110,15 +145,16 @@ def model_3_2d(formula,bandgap_pbe):
 def main_ML_2d(formula,bandgap_pbe):
     if  formula:
         if bandgap_pbe:
-
             model_3_2d(formula,bandgap_pbe)
             print('The model has score=0.94,RMSE=0.36')
         else:
-
-            model_1_2d(formula)
-            print('The model has score=0.73,RMSE=0.80')
+            res=model_classifier_2d(formula)
+            if res>0:
+                model_1_2d(formula)
+                print('The model has score=0.73,RMSE=0.80')
+            else:
+                print('formula : {}\n Bandgap: 0'.format(formula))
     else:
-
         model_2_2d(bandgap_pbe)
         print('The model has score=0.87,RMSE=0.54')
 
@@ -131,7 +167,7 @@ def main_ML(dimension,formula,bandgap_pbe):
 if __name__ == '__main__':
     dimension=input('dimension (2d or 3d) : ')
     formula=input('formula : ')
-    bandgap_pbe=input('bandgap_gga : ')
+    bandgap_pbe=input('bandgap_pbe : ')
     main_ML(dimension,formula,bandgap_pbe)
 
 
